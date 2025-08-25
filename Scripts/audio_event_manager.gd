88981b2bb498cd46741262a4bffc2a608b8e2d4b
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2025-08-25 16:23:27 -0400

    stuff I'm working at

---
+++ Scripts/audio_event_manager.gd
@@ -0,0 +1,7 @@
+## the manager for audio events. pulls from the ghost manager for specifics
+class_name AudioEventManager extends Node
+
+
+func play_sound() -> void:
+	
+	pass
---
+++ Scripts/water.gd
@@ -0,0 +1,16 @@
+extends Area3D
+class_name WaterSlow
+
+@export var _is_deep_water : bool = false
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		# run the helper function to reduce the players speed
+		if _is_deep_water:
+			# run the helper function to extinguish torch
+			pass
+		pass
