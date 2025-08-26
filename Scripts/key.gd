4ac105a4cfe7c02ce87aad21e4b5fb4d86d84c0a
Matthew Gray <phlip45@gmail.com>
2025-08-26 00:41:07 -0700

    🗝 goes up and down now

--- Scripts/key.gd
+++ Scripts/key.gd
@@ -1,5 +1,7 @@
 extends BaseCollectable
 
+@onready var key: Node3D = $key
+
 ## there are only 3 keys, maybe we make those keys further inherited scenes
 @export var _key_num : int:
 	get:
@@ -7,10 +9,13 @@ extends BaseCollectable
 	set(val):
 		_key_num = clampi(val, 0,2)
 		_key_num = val
-
+var bob_amplitude: float = 1.0
+var bob_freq: float= 4.0
 var 	rotation_speed: float = 1.4
-
+var bob_bucket:float = 0
 func _process(delta: float) -> void:
+	bob_bucket = fmod(bob_bucket + delta*bob_freq, PI*2)
+	key.position.y = sin(bob_bucket) * bob_amplitude
 	global_rotate(Vector3.UP, rotation_speed * delta)	
 
 func do_pickup_action() -> void:
