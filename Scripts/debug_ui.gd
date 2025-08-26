40a83a5394976a9998b35b4209a517187e739bd3
Lýz <147095077+Lyzurly@users.noreply.github.com>
2025-08-25 19:27:04 -0500

    debug is setup, player has head, player script cleaned up

--- Scripts/debug_ui.gd
+++ Scripts/debug_ui.gd
@@ -1,5 +1,16 @@
 extends CanvasLayer
 
-@onready var progress_danger_percent: ProgressBar = $MarginContainer/VBoxContainer/Debug_DangerPercent/VBoxContainer/ProgressBar
+@onready var progress_danger_percent: ProgressBar = %Progress_DangerPercent
 
-@onready var progress_danger_level: ProgressBar = $MarginContainer/VBoxContainer/Debug_DangerPercent/VBoxContainer/ProgressBar
+@onready var progress_danger_level: ProgressBar = %Progress_DangerLevel
+
+func _ready() -> void:
+	GhostManager.ref.danger_level_changed.connect(_on_danger_level_changed)
+	
+func _on_danger_level_changed(level: int) -> void:
+	progress_danger_level.value = level
+
+func _process(delta: float) -> void:
+	progress_danger_percent.value = GhostManager.ref.get_danger_percentage()
+	
+	
